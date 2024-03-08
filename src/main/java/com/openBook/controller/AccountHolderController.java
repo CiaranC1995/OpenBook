3dbe454c81280d48f660ff64a5ab07f02b748a5e
Ciaran Casey <ccasee@hotmail.co.uk>
2024-03-08 14:57:46 +0000

    OBD-37 Implement SLF4J to controller classes, upgraded spring boot version as well and fixed a couple of unit tests.

--- src/main/java/com/openBook/controller/AccountHolderController.java
+++ src/main/java/com/openBook/controller/AccountHolderController.java
@@ -5,6 +5,8 @@ import com.openBook.dto.AccountHolderDto;
 import com.openBook.service.AccountHolderService;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping(value = {"/accountHolders", "/accountHolders/"})
 public class AccountHolderController {
+    private static final Logger log = LoggerFactory.getLogger(AccountHolderController.class);
 
     private final AccountHolderService accountHolderService;
 
@@ -26,8 +29,10 @@ public class AccountHolderController {
     public ResponseEntity<List<AccountHolderDto>> getAllAccountHolders() {
         List<AccountHolderDto> accountHolders = accountHolderService.getAllAccountHolders();
         if (accountHolders.isEmpty()) {
+            log.info("No account holders exist ...");
             return ResponseEntity.notFound().build();
         }
+        log.info("Returning {} account holder(s) ...", accountHolders.size());
         return ResponseEntity.ok(accountHolders);
     }
 
@@ -36,8 +41,10 @@ public class AccountHolderController {
         AccountHolderDto accountHolders = accountHolderService.getAccountHolderById(id)
                 .orElse(null);
         if (accountHolders == null) {
+            log.info("No account holder with id={} exists ...", id);
             return ResponseEntity.notFound().build();
         }
+        log.info("Returning account holder with id={} ...", id);
         return ResponseEntity.ok(accountHolders);
     }
 
@@ -49,17 +56,23 @@ public class AccountHolderController {
         List<AccountHolderDto> accountHolders;
 
         if (firstName != null && lastName != null) {
+            log.info("Searching for account holder(s) named firstName={} lastName={} ...", firstName, lastName);
             accountHolders = accountHolderService.getAccountHoldersByFirstNameAndLastName(firstName, lastName);
         } else if (firstName != null) {
+            log.info("Searching for account holder(s) named firstName={} ...", firstName);
             accountHolders = accountHolderService.getAccountHoldersByFirstName(firstName);
         } else if (lastName != null) {
+            log.info("Searching for account holder(s) named lastName={} ...", lastName);
             accountHolders = accountHolderService.getAccountHoldersByLastName(lastName);
         } else {
+            log.warn("Bad request, could not process request parameter(s) ...");
             return ResponseEntity.badRequest().build();
         }
         if (accountHolders.isEmpty()) {
+            log.info("No matching account holder(s) exist ...");
             return ResponseEntity.notFound().build();
         }
+        log.info("Returning {} account holder(s) ...", accountHolders.size());
         return ResponseEntity.ok(accountHolders);
     }
 
@@ -67,22 +80,28 @@ public class AccountHolderController {
     public ResponseEntity<List<AccountHolderDto>> getAccountHoldersByEmail(@RequestParam String email) {
 
         if (email == null) {
+            log.warn("Bad request, could not process request parameter(s) ...");
             return ResponseEntity.badRequest().build();
         }
         List<AccountHolderDto> accountHolders = accountHolderService.getAccountHoldersByEmail(email);
 
         if (accountHolders.isEmpty()) {
+            log.info("No account holder(s) with email={} exist ...", email);
             return ResponseEntity.notFound().build();
         }
+        log.info("Returning {} account holder(s) ...", accountHolders.size());
         return ResponseEntity.ok(accountHolders);
     }
 
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<String> createAccountHolder(@RequestBody @Valid AccountHolderCreationDto accountHolderCreationDto) {
         try {
+            log.info("Attempting to create account holder with id={} ...", accountHolderCreationDto.getId());
             AccountHolderCreationDto createdAccountHolder = accountHolderService.createAccountHolder(accountHolderCreationDto);
-            return ResponseEntity.ok(String.format("Account holder with id=%s successfully created...", createdAccountHolder.getId()));
+            log.info("Account holder with with id={} successfully created ...", createdAccountHolder.getId());
+            return ResponseEntity.ok(createdAccountHolder.toString());
         } catch (IllegalArgumentException e) {
+            log.warn("Failed to create account holder with id={} ...", accountHolderCreationDto.getId());
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
@@ -90,13 +109,16 @@ public class AccountHolderController {
     @RequestMapping(value = {"/{id}", "/{id}/"}, method = RequestMethod.PUT)
     public ResponseEntity<String> updateAccountHolder(@PathVariable Long id, @RequestBody @Valid AccountHolderDto accountHolderDto) {
         if (accountHolderService.getAccountHolderById(id).isEmpty()) {
+            log.warn("Account holder with id={} does not exist ...", id);
             return ResponseEntity.notFound().build();
         }
 
         try {
             AccountHolderDto updatedAccountHolder = accountHolderService.updateAccountHolder(id, accountHolderDto);
-            return ResponseEntity.ok(String.format("Account holder with id=%s successfully updated...", updatedAccountHolder.getId()));
+            log.info("Account holder with with id={} successfully updated ...", id);
+            return ResponseEntity.ok(updatedAccountHolder.toString());
         } catch (IllegalArgumentException e) {
+            log.warn("Bad request, could not process request parameter(s) ...");
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
@@ -104,16 +126,21 @@ public class AccountHolderController {
     @RequestMapping(value = {"/delete/{id}", "/delete/{id}/"}, method = RequestMethod.DELETE)
     public ResponseEntity<String> deleteAccountHolder(@PathVariable Long id) {
         try {
+            log.info("Attempting to delete account holder with id={} ...", id);
             accountHolderService.deleteAccountHolder(id);
-            return ResponseEntity.ok(String.format("Account holder with id=%s successfully deleted...", id));
+            log.info("Successfully deleted account holder with id={} ...", id);
+            return ResponseEntity.ok().build();
         } catch (EntityNotFoundException e) {
+            log.warn("Deletion attempt unsuccessful, no account holder with id={} exists ...", id);
             return ResponseEntity.notFound().build();
         }
     }
 
     @RequestMapping(value = {"/delete", "/delete/"}, method = RequestMethod.DELETE)
     public ResponseEntity<String> deleteAllAccountHolders() {
+        log.info("Attempting to delete all account holders ...");
         accountHolderService.deleteAllAccountHolders();
-        return ResponseEntity.ok().body("All account holders successfully deleted...");
+        log.info("All account holders successfully deleted ...");
+        return ResponseEntity.ok().build();
     }
 }
